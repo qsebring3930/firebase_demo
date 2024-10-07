fc89f1eb6f14920933ea51cc86b9a23af0572a47
Thomas <quentin.sebring@icloud.com>
2024-10-07 14:57:37 -0500

    Sstep 6 completed

--- flutter_demo/lib/home_page.dart
+++ flutter_demo/lib/home_page.dart
@@ -10,6 +10,7 @@ import 'package:provider/provider.dart';          // new
 import 'app_state.dart';                          // new
 import 'src/authentication.dart';                 // new
 import 'src/widgets.dart';
+import 'guest_book.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -45,6 +46,20 @@ class HomePage extends StatelessWidget {
           const Header("What we'll be doing"),
           const Paragraph(
             'Join us for a day full of Firebase Workshops and Pizza!',
+          ),
+          Consumer<ApplicationState>(
+            builder: (context, appState, _) => Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                if (appState.loggedIn) ...[
+                  const Header('Discussion'),
+                  GuestBook(
+                    addMessage: (message) =>
+                        appState.addMessageToGuestBook(message),
+                  ),
+                ],
+              ],
+            ),
           ),
         ],
       ),
